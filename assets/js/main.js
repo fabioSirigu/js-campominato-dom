11d59569e6aba552d034af5340f6095c6f48823c
naz1ne <fabio.sirigu.1995@gmail.com>
2022-10-18 16:32:24 +0200

    refactoring and cleaning code

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,10 @@ playButton.addEventListener('click', function(){
       generateGrid(gridContainer, cellNumber);  
 });
 
+// genero le bombe
+const bombs = generateBomb(1, level);
+// -log in console i numeri random
+console.log(bombs, 'questo è l array');
 
 function generateGrid (where, howMany){
       for (let i = 1; i <= howMany; i++) {
@@ -30,47 +34,37 @@ function generateGrid (where, howMany){
                   cellElement.style.width = 'calc(100% / 7)';
             }
       }
-      // genero le bombe
-      const bombs = generateBomb(1, level);
-      // -log in console i numeri random
-      console.log(bombs, 'questo è l array');
-      // quando clicco su ogni casella (quindi dentro un ciclo for con la lenght), la cella cliccata si colora ed emette un messaggio in console col numero della cella cliccata
-      // mi faccio una variabile per selezionare la casella
-      // quando clicco la casella, aggiungo una classe per il colore (o cambio il colore dirett da JS)
+
       const cell = document.querySelectorAll('.cell');
+      let counter = 0
       for (let i = 0; i < cell.length; i++){
             const singleCell = cell[i];
             
-            
-            let counter = 0
             singleCell.addEventListener('click', function () {
-                  // se il valore di singleCell contiene un valore presente nell'array bombs allora cambio il background
                   const cellNumber = singleCell.innerHTML;
                   console.log(cellNumber);
                   
-                  singleCell.classList.add('background'); //uso toggle per mettere e rimuovere la classe al click
-                  /* console.log(`la cella è la numero ${singleCell.innerHTML}`); //.innerHTML mi stampa il contenuto della singleCell */
+                  singleCell.classList.add('background');
+                  bombOrNot(cellNumber, singleCell)
                   
-                  // faccio un ciclo for per scorrere dentro bombs[] e prendere il suo valore da poter comparare con il valore di cellNumber
-                  // In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
-                  for (let i = 0; i < bombs.length; i++) {
-                        const bomb = bombs[i];
-                        /* console.log(bomb, 'sei qui'); */
-                        if (cellNumber == bomb){
-                              
-                              singleCell.classList.add('explosion'); //uso toggle per mettere e rimuovere la classe al click
-                              if(!alert('hai perso!')){window.location.reload();}
-                        }
-                  }
-                  counter += 1
-	            const displayCount = document.querySelector('.counter');
-
+                  counter += 1;
+                  const displayCount = document.querySelector('.counter');
                   displayCount.innerHTML = counter;
             })
       }   
 }
-
-
+function bombOrNot(number, position){
+      for (let i = 0; i < bombs.length; i++) {
+            const bomb = bombs[i];
+            /* console.log(bomb, 'sei qui'); */
+            if (number == bomb){
+                  
+                  position.classList.add('explosion'); //uso toggle per mettere e rimuovere la classe al click
+                  if(!alert('hai perso!')){window.location.reload();}
+            }
+      }
+}
+      
 
 
 function generateBomb(min, max){
